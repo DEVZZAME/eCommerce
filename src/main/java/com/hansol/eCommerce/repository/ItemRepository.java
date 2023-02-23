a485f41171d43392c82f94be72993e88b54a303a
DEVZZAME <92978598+DEVZZAME@users.noreply.github.com>
2023-02-24 02:34:34 +0900

    #2 Query Method Test

--- src/main/java/com/hansol/eCommerce/repository/ItemRepository.java
+++ src/main/java/com/hansol/eCommerce/repository/ItemRepository.java
@@ -3,6 +3,16 @@ package com.hansol.eCommerce.repository;
 import com.hansol.eCommerce.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ItemRepository extends JpaRepository<Item, Long> {
+import java.util.List;
 
+public interface ItemRepository extends JpaRepository<Item, Long> {
+    List<Item> findByItemNm(String itemNn);
+
+    List<Item> findByItemNmOrItemDetail(String itemNm, String itemDetail);
+
+    List<Item> findByPriceLessThan(Integer price);
+
+    List<Item> findByPriceLessThanOrderByPriceDesc(Integer price);
+
+    List<Item> findByPriceGreaterThan(Integer price);
 }
