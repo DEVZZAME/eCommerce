5383299f33a19409cf0e7c5edc04f2db61fe21a1
DEVZZAME <92978598+DEVZZAME@users.noreply.github.com>
2023-02-26 03:31:09 +0900

    #5 주문 관련 Entity, Repository, Constant 생성

---
+++ src/main/java/com/hansol/eCommerce/constant/OrderStatus.java
@@ -0,0 +1,5 @@
+package com.hansol.eCommerce.constant;
+
+public enum OrderStatus {
+    ORDER, CANCEL
+}
---
+++ src/main/java/com/hansol/eCommerce/entity/Order.java
@@ -0,0 +1,37 @@
+package com.hansol.eCommerce.entity;
+
+import com.hansol.eCommerce.constant.OrderStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+@Getter
+@Setter
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    private LocalDateTime orderDate;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    private LocalDateTime regTime;
+
+    private LocalDateTime updateTime;
+}
